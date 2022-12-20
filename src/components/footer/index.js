9e016ed25bbe79c5fc559b9d4a103cc5032d7914
c-grigsby <grigsby.c@outlook.com>
2022-12-19 19:50:07 -0500

    feat(loginPage): add login form

---
+++ src/components/footer/index.js
@@ -0,0 +1,21 @@
+// @packages
+import { Link } from 'react-router-dom';
+import React from 'react';
+// @scripts
+import './style.css';
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <div className='footer_wrapper'>
+        <Link to='/'>Privacy</Link>
+        <Link to='/'>Terms</Link>
+        <Link className='rooster_copy' to='/' style={{ fontSize: '12px' }}>
+          Rooster &copy; 2022
+        </Link>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
