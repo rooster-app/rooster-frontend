48434786483e213670a8cdd3beea089301b0b580
Christopher Grigsby <grigsby.c@outlook.com>
2022-12-20 19:53:29 -0500

    feat(homePage): add createPost & links (#7)
    * feat(homePage): add createPost & links
    
    * feat(loginPage): mod responsive styles

---
+++ src/components/home/right/index.js
@@ -0,0 +1,33 @@
+// @scripts
+import Contact from "./Contact";
+import { Dots, NewRoom, Search } from "../../../svg";
+import "./style.css";
+
+export default function RightHome({ user }) {
+  const color = "#65676b";
+  return (
+    <div className="right_home">
+      <div className="heading">Rooster</div>
+      <div className="splitter1"></div>
+      <div className="contacts_wrap">
+        <div className="contacts_header">
+          <div className="contacts_header_left">Contacts</div>
+          <div className="contacts_header_right">
+            <div className="contact_circle hover1">
+              <NewRoom color={color} />
+            </div>
+            <div className="contact_circle hover1">
+              <Search color={color} />
+            </div>
+            <div className="contact_circle hover1">
+              <Dots color={color} />
+            </div>
+          </div>
+        </div>
+        <div className="contacts_list">
+          <Contact user={user} />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
+// @packages
+import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import "./styles/icons/icons.css";
-import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore } from "redux";
+// @scripts
+import "./index.css";
+import "./styles/icons/icons.css";
 import rootReducer from "./reducers";
+
 const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
