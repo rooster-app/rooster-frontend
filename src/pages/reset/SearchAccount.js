1c0eb2df6a1d2a91be76ee2ddb45f75570edb95b
Christopher Grigsby <grigsby.c@outlook.com>
2023-05-24 20:50:35 -0400

    feat(reset): mod error msg

--- src/pages/reset/SearchAccount.js
+++ src/pages/reset/SearchAccount.js
@@ -18,7 +18,7 @@ export default function SearchAccount({
 
   const validateEmail = Yup.object({
     email: Yup.string()
-      .required("Email address ir required.")
+      .required("Email address is required.")
       .email("Must be a valid email address.")
       .max(50, "Email address can't be more than 50 characters."),
   });
